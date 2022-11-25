b4f2bb12d75415ff10074799d3f154ea6dac08ee
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2022-11-25 18:04:39 -0500

    train script

--- MultinomialNaiveBayes/train_mnb.py
+++ MultinomialNaiveBayes/train_mnb.py
@@ -1,5 +1,28 @@
 
 from argparse import ArgumentParser
 import json
+import os
+
+from mnbutils import SymbolMultinomialNaiveBayesExtractor
 
 
+def get_argparser():
+    argparser = ArgumentParser(description='Train a multinomial naive Bayes model for symbol extraction')
+    argparser.add_argument('symbolinfopath', help='path of all symbol information (*.json)')
+    argparser.add_argument('modeldir', help='path of the directory for the model files')
+    argparser.add_argument('--alpha', type=float, default=1., help='alpha for Laplace smoothing (default: 1.0)')
+    argparser.add_argument('--gamma', type=float, default=0.75, help='gamma for decay matching (gamma: 0.75)')
+    return argparser
+
+
+if __name__ == '__main__':
+    args = get_argparser().parse_args()
+    if not os.path.isdir(args.modeldir):
+        os.makedirs([args.modeldir])
+    symbolinfo = json.load(open(args.symbolinfopath, 'r'))
+
+    extractor = SymbolMultinomialNaiveBayesExtractor(alpha=args.alpha, gamma=args.gamma)
+    for info in symbolinfo:
+        extractor.ingest_one_symbol_info(info)
+    extractor.train()
+    extractor.save_model(args.modeldir)
